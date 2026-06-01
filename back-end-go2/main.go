7b0114bf14165923741b63b86be79f4925957d9d
yurimutz <yurimutz02@gmail.com>
2026-06-01 20:22:54 -0300

    fix: separando as imagens go para acessar folders diferentes

---
+++ back-end-go2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// fmt.Println("Hello, World!")
+	log.Println("Servidor de Origem pronto e escutando na porta 8082...")
+	log.Fatal(http.ListenAndServe(":8082", nil))
+}
