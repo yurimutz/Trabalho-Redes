d70dee42995e7096ec21a62a4b8b6744624e5339
RafaBaker <rafael1309mt@gmail.com>
2026-06-02 20:35:31 -0300

    feat: atualização no docker-compose e começo da pagina principal

--- cliente/src/App.vue
+++ cliente/src/App.vue
@@ -1,8 +1,7 @@
 <script setup>
-  import { ref, onMounted, onBeforeUpdate } from 'vue'
+  import { onMounted } from 'vue';
 
-  import HelloWorld from './components/HelloWorld.vue'
-  import TheWelcome from './components/TheWelcome.vue'
+  import HelloWorld from './components/HelloWorld.vue';
 
   const url = "http://localhost:8080/manifesto/manifesto.mpd";
 
@@ -57,13 +56,13 @@
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Consegui" />
     </div>
   </header>
 
-  <main>
+  <!-- <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
 <style scoped>
