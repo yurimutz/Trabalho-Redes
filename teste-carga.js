34a9d0ab1ce344993a7c2b12a38f63e9e6b03b40
RafaBaker <rafael1309mt@gmail.com>
2026-06-11 11:08:04 -0300

    mudanças nos testes para o video do santicazorla

--- teste-carga.js
+++ teste-carga.js
@@ -1,5 +1,5 @@
-import http from 'k6/http';
 import { check, sleep } from 'k6';
+import http from 'k6/http';
 
 // Configuração da "Força" do Teste
 export const options = {
@@ -8,6 +8,8 @@ export const options = {
     // aumentamos a duração do teste para dar tempo de todos terminarem o ciclo
     duration: '1m30s',
 };
+
+const HOSTNAME = __ENV.HOSTNAME || 'localhost';
 
 export default function () {
     // PASSO 1: O utilizador abre o site e pede o Catálogo
@@ -18,10 +20,10 @@ export default function () {
     });
 
     // O utilizador demora 2 segundos a olhar para o catálogo
-    sleep(2);
+    sleep(2);t
 
     // PASSO 2: O utilizador dá Play num vídeo!
-    let resManifesto = http.get('http://192.168.100.111:8080/videos/lol/manifesto.mpd');
+    let resManifesto = http.get('http://192.168.100.111:8080/videos/Santi Cazorla, quando futebol vira arte./manifesto.mpd');
 
     check(resManifesto, {
         'Manifesto baixado': (r) => r.status === 200,
@@ -30,9 +32,9 @@ export default function () {
     // PASSO 3: O reprodutor (DASH) pede 10 pedaços de vídeo
     for (let i = 1; i <= 10; i++) {
         // Monta o nome do chunk dinamicamente (ex: slice_0_1.m4s, slice_0_2.m4s)
-        let pedaco = `slice_4_${i}.m4s`;
+        let pedaco = `chunk_1_${i}.m4s`;
 
-        let resChunk = http.get(`http://192.168.100.111:8080/videos/lol/${pedaco}`);
+        let resChunk = http.get(`http://192.168.100.111:8080/videos/Santi Cazorla, quando futebol vira arte./${pedaco}`);
 
         check(resChunk, {
             'Pedaco baixado': (r) => r.status === 200 || r.status === 206,
@@ -40,11 +42,11 @@ export default function () {
 
         // O player espera 4 segundos exatos antes de pedir o próximo pedaço
     }
-    for (let i = 1; i <= 9; i++) {
+    for (let i = 1; i <= 5; i++) {
         sleep(4);
-        let pedaco = `slice_4_${i}.m4s`;
+        let pedaco = `chunk_1_${i}.m4s`;
 
-        let resChunk = http.get(`http://192.168.100.111:8080/videos/lol/${pedaco}`);
+        let resChunk = http.get(`http://192.168.100.111:8080/videos/Santi Cazorla, quando futebol vira arte./${pedaco}`);
 
         check(resChunk, {
             'Pedaco baixado': (r) => r.status === 200 || r.status === 206,
